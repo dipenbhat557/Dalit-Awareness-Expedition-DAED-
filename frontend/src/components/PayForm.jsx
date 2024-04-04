057d70e4d2bcf87ac47c23b081e02bf8a3201b62
pawan2061 <pandeypawan2061@gmail.com>
2024-04-04 21:44:59 +0530

    :memo: fix supportus and contact page

--- frontend/src/components/PayForm.jsx
+++ frontend/src/components/PayForm.jsx
@@ -1,6 +1,6 @@
 const PayForm = () => {
   return (
-    <div className=" max-w-[400px]  mt-8 p-4">
+    <div className=" max-w-[400px]  mt-8 p-4 items-center">
       <form className="  my-2   ">
         <div class="mb-5 w-full">
           <label
@@ -35,7 +35,7 @@ const PayForm = () => {
             for="base-input"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Adress *
+            Current Adress *
           </label>
           <input
             type="text"
@@ -49,7 +49,7 @@ const PayForm = () => {
             for="base-input"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Message *
+            Please mention the type of inkind support *
           </label>
           <textarea
             type="text"
@@ -57,6 +57,34 @@ const PayForm = () => {
             class="block h-32  w-full  text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-m focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
+
+        <div className="my-8">
+          <h1 className="text-gray-900 ">
+            For cash support ,please deposit in our bank account
+          </h1>
+          <ul className="list-disc">
+            <li>Account Name</li>
+            <li>Account Number</li>
+            <li>Bank Name: Nepal Investment Bank Ltd</li>
+            <li>Bank Adress: Durbar Marg,Kathmandu ,Nepal</li>
+            <li>SWIFT CODE:</li>
+          </ul>
+        </div>
+        <div>
+          <label
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            for="file_input"
+          >
+            Upload slip deposit here
+          </label>
+
+          <input
+            className="block w-full text-sm text-gray-900  cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            id="file_input"
+            type="file"
+          />
+        </div>
+
         <div className="my-4">
           <h1>Mandatory fields*</h1>
         </div>
